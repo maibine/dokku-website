e044a7fef2ddcb433a8f7384a353d816bc34172e
maibine <adi.gheorghica@gmail.com>
2024-05-24 11:36:59 +0300

    change title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Localized website',
+  title: 'Localized website test',
   tagline: 'Check the site in other language',
   favicon: 'img/favicon.ico',
 
