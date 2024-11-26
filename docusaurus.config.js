5dcd9dbbe4f0c2ed01404ab20970ba24a6a49b42
Adi <adrian.g@fluentis.com>
2024-11-26 10:57:34 +0200

    cloudflare test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,13 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  //url: 'https://github.com',
+  //baseUrl: '/dokku-website/',
+  url: 'https://dokku-website.pages.dev',
+  baseUrl: '/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/dokku-website/',
+  
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
